cd7e0f27dad419e50f4fe5f417d9d2bf72cfb47e
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-07-25 16:11:37 +0200

    change

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,19 +1,3 @@
 console.log("bonjour");
 
-let messages = document.getElementById("messages").querySelectorAll("li");
-console.log(messages);
-messages.forEach((card) => {
-  card.addEventListener("click", (event) => {
-    if (event.currentTarget.querySelector(".message").classList.value === "message active") {
-      event.currentTarget.querySelector(".message").classList.remove("active");
-    }
-    else {
-      document.querySelectorAll(".message").forEach((item) =>{
-        item.classList.remove("active");
-      });
-      event.currentTarget.querySelector(".message").classList.add("active");
-    }
-  });
-});
-
 //event.currentTarget.querySelector(".message").classList.value === "message active";
