46c0858b5f7061ae79d29ee103ced3850a574ef6
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-07-25 16:51:59 +0200

    change

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,41 +1,112 @@
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
+//circle start
+let progressBar = document.querySelector('.e-c-progress');
+let indicator = document.getElementById('e-indicator');
+let pointer = document.getElementById('e-pointer');
+let length = Math.PI * 2 * 100;
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+progressBar.style.strokeDasharray = length;
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
+function update(value, timePercent) {
+  var offset = - length - length * value / (timePercent);
+  progressBar.style.strokeDashoffset = offset;
+  pointer.style.transform = `rotate(${360 * value / (timePercent)}deg)`;
+};
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+//circle ends
+const displayOutput = document.querySelector('.display-remain-time')
+const pauseBtn = document.getElementById('pause');
+const setterBtns = document.querySelectorAll('button[data-setter]');
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+let intervalTimer;
+let timeLeft;
+let wholeTime = 0.5 * 60; // manage this to set the whole time
+let isPaused = false;
+let isStarted = false;
+
+
+update(wholeTime,wholeTime); //refreshes progress bar
+displayTimeLeft(wholeTime);
+
+function changeWholeTime(seconds){
+  if ((wholeTime + seconds) > 0){
+    wholeTime += seconds;
+    update(wholeTime,wholeTime);
   }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+for (var i = 0; i < setterBtns.length; i++) {
+    setterBtns[i].addEventListener("click", function(event) {
+        var param = this.dataset.setter;
+        switch (param) {
+            case 'minutes-plus':
+                changeWholeTime(1 * 60);
+                break;
+            case 'minutes-minus':
+                changeWholeTime(-1 * 60);
+                break;
+            case 'seconds-plus':
+                changeWholeTime(1);
+                break;
+            case 'seconds-minus':
+                changeWholeTime(-1);
+                break;
+        }
+      displayTimeLeft(wholeTime);
+    });
+}
+
+function timer (seconds){ //counts time, takes seconds
+  let remainTime = Date.now() + (seconds * 1000);
+  displayTimeLeft(seconds);
+
+  intervalTimer = setInterval(function(){
+    timeLeft = Math.round((remainTime - Date.now()) / 1000);
+    if(timeLeft < 0){
+      clearInterval(intervalTimer);
+      isStarted = false;
+      setterBtns.forEach(function(btn){
+        btn.disabled = false;
+        btn.style.opacity = 1;
+      });
+      displayTimeLeft(wholeTime);
+      pauseBtn.classList.remove('pause');
+      pauseBtn.classList.add('play');
+      return ;
+    }
+    displayTimeLeft(timeLeft);
+  }, 1000);
+}
+function pauseTimer(event){
+  if(isStarted === false){
+    timer(wholeTime);
+    isStarted = true;
+    this.classList.remove('play');
+    this.classList.add('pause');
+
+    setterBtns.forEach(function(btn){
+      btn.disabled = true;
+      btn.style.opacity = 0.5;
+    });
+
+  }else if(isPaused){
+    this.classList.remove('play');
+    this.classList.add('pause');
+    timer(timeLeft);
+    isPaused = isPaused ? false : true
+  }else{
+    this.classList.remove('pause');
+    this.classList.add('play');
+    clearInterval(intervalTimer);
+    isPaused = isPaused ? false : true ;
+  }
+}
+
+function displayTimeLeft (timeLeft){ //displays time on the input
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let displayString = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  displayOutput.textContent = displayString;
+  update(timeLeft, wholeTime);
+}
+
+pauseBtn.addEventListener('click',pauseTimer);
